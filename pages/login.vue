edd8d8ddd10ca48044501cc9f46513cf0922e2dc
tx <302871586@qq.com>
2020-05-11 21:56:38 +0800

    布局登录页

---
+++ pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="12" :offset="3">
+        <a href="https://www.mi.com/index.html" class="logo"></a>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6" :offset="14">
+        <div class="loginForm"></div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import url("@/assets/css/login/login.css");
+</style>
