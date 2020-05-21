1951e204ad4204774b4420e04c17231db9c24ccd
tx <302871586@qq.com>
2020-05-21 21:25:44 +0800

    完善注册功能

--- pages/Register.vue
+++ pages/Register.vue
@@ -20,18 +20,16 @@
             <el-form :model="ruleForm"
                      status-icon
                      :rules="rules"
-                     ref="ruleForm"
+                     ref="phoneForm"
                      class="demo-ruleForm">
-              <el-form-item prop="account">
-                <el-input v-model="ruleForm.account"
+              <el-form-item prop="phoneAccount">
+                <el-input v-model="ruleForm.phoneAccount"
                           autocomplete="off"
-                          placeholder="手机/邮箱"></el-input>
-
-                <span class="status">{{ statusMsg }}</span>
-              </el-form-item>
-              <el-form-item>
-                <el-button style="width:100%;"
-                           @click="sendMsg">发送验证码</el-button>
+                          placeholder="手机"></el-input>
+                <el-button size="mini"
+                           round
+                           @click="sendPhoneMsg">发送验证码</el-button>
+                <span class="status">{{ phoneStatusMsg }}</span>
               </el-form-item>
               <el-form-item prop="verificationCode">
                 <el-input v-model="ruleForm.verificationCode"
@@ -53,7 +51,7 @@
               <el-form-item>
                 <el-button type="danger"
                            style="width: 100%;"
-                           @click="login('ruleForm')">立即注册</el-button>
+                           @click="phoneRegister('phoneForm')">立即注册</el-button>
                 <p class="aboutAccount">
                   <a href="/login"
                      class="register">用户密码登录</a>
@@ -65,9 +63,51 @@
           <el-tab-pane>
             <span slot="label"
                   class="registerTab">
-              <i class="el-icon-full-screen"></i>
-              扫描注册
+              <i class="el-icon-message"></i>
+              邮箱注册
             </span>
+            <el-form :model="ruleForm"
+                     status-icon
+                     :rules="rules"
+                     ref="emailForm"
+                     class="demo-ruleForm">
+              <el-form-item prop="emailAccount">
+                <el-input v-model="ruleForm.emailAccount"
+                          autocomplete="off"
+                          placeholder="邮箱"></el-input>
+                <el-button size="mini"
+                           round
+                           @click="sendEmailMsg">发送验证码</el-button>
+                <span class="status">{{ emailStatusMsg }}</span>
+              </el-form-item>
+              <el-form-item prop="verificationCode">
+                <el-input v-model="ruleForm.verificationCode"
+                          autocomplete="off"
+                          placeholder="请输入验证码"></el-input>
+              </el-form-item>
+              <el-form-item prop="password">
+                <el-input type="password"
+                          v-model="ruleForm.password"
+                          autocomplete="off"
+                          placeholder="密码"></el-input>
+              </el-form-item>
+              <el-form-item prop="rePassword">
+                <el-input type="password"
+                          v-model="ruleForm.rePassword"
+                          autocomplete="off"
+                          placeholder="确认密码"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="danger"
+                           style="width: 100%;"
+                           @click="emailRegister('emailForm')">立即注册</el-button>
+                <p class="aboutAccount">
+                  <a href="/login"
+                     class="register">用户密码登录</a>
+                  <a class="noCode">收不到验证码？</a>
+                </p>
+              </el-form-item>
+            </el-form>
           </el-tab-pane>
         </el-tabs>
       </el-col>
@@ -80,24 +120,43 @@ export default {
   name: "login",
   data () {
     return {
-      statusMsg: "",
+      phoneStatusMsg: "",
+      emailStatusMsg: "",
       ruleForm: {
-        account: "",
+        phoneAccount: "",
+        emailAccount: "",
         verificationCode: "",
-        password: ""
+        password: "",
+        rePassword: ""
       },
+      timerid: "",
       rules: {
-        account: [
+        phoneAccount: [
           {
             require: true,
             trigger: ["blur"],
             validator: (rule, value, callback) => {
               let phone = /^(((13[0-9]{1})|(15[0-9]{1})|(16[0-9]{1})|(17[3-8]{1})|(18[0-9]{1})|(19[0-9]{1})|(14[5-7]{1}))+\d{8})$/;
+              if (value === "") {
+                callback(new Error("请输入账号！"));
+              } else if (!phone.test(value)) {
+                callback(new Error("账号不是手机号！"));
+              } else {
+                callback();
+              }
+            }
+          }
+        ],
+        emailAccount: [
+          {
+            require: true,
+            trigger: ["blur"],
+            validator: (rule, value, callback) => {
               let email = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
               if (value === "") {
                 callback(new Error("请输入账号！"));
-              } else if (!phone.test(value) && !email.test(value)) {
-                callback(new Error("账号不是手机号或者邮箱！"));
+              } else if (!email.test(value)) {
+                callback(new Error("账号不是者邮箱！"));
               } else {
                 callback();
               }
@@ -149,10 +208,52 @@ export default {
     };
   },
   methods: {
-    sendMsg () {
-      
+    sendPhoneMsg () {
+      if (this.timerid) {
+        return false;
+      }
+      this.$refs["phoneForm"].validateField("phoneAccount", (valid) => {
+        if (!valid) {
+          this.$axios.post("/register/registerPhone", {
+            phone: this.ruleForm.phoneAccount
+          }).then(({ status, data }) => {
+            console.log(status, data);
+            if (!status) {
+              this.$message.error('获取失败');
+              return;
+            }
+            let count = 60;
+            this.phoneStatusMsg = `验证码已发送，剩余${--count}s`;
+            this.timerid = setInterval(() => {
+              this.phoneStatusMsg = `验证码已发送，剩余${--count}s`;
+              if (count === 0) {
+                clearInterval(this.timerid)
+              }
+            }, 1000);
+            this.$message({
+              showClose: true,
+              message: '验证码：' + data.codeNum,
+              type: 'success',
+              duration: 20000
+            });
+          })
+        }
+      })
     },
-    login (formName) {
+    sendEmailMsg () {
+
+    },
+    phoneRegister (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$message("submit!");
+        } else {
+          this.$message.error("error submit!!");
+          return false;
+        }
+      });
+    },
+    emailRegister () {
       this.$refs[formName].validate(valid => {
         if (valid) {
           this.$message("submit!");
--- server/interface/register.js
+++ server/interface/register.js
@@ -1,16 +1,18 @@
 import Router from "koa-router";
+import Redis from "koa-redis";
 import axios from "./utils/axios.js";
 import mds from "crypto-js/md5";
 import register from "../dbs/models/register";
 let router = new Router({
   prefix: "/register"
 });
-router.get("/registerPhone", async ctx => {
-  let phone = ctx.request.body;
+let Store = new Redis().client;
+router.post("/registerPhone", async ctx => {
+  let phone = ctx.request.body.phone;
   let time = new Date();
   ctx.body = {
-    code: 0,
-    codeNum: ParseInt(Math.random() * 100000)
+    code: 1,
+    codeNum: parseInt(Math.random() * 1000000)
   };
 });
-export default router
+export default router;
